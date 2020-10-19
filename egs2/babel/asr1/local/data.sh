34894fa766233e4b73df542900b66dcd30ff47fa
Zhaomou Song <zs323@cam.ac.uk>
2020-10-19 17:18:52 +0100

    changed data.sh

--- egs2/babel/asr1/local/data.sh
+++ egs2/babel/asr1/local/data.sh
@@ -15,11 +15,12 @@ log() {
 
 langs="106"
 recog="106"
+BABEL="BABEL"
 
 for l in ${langs} ${recog}; do
-  echo "${"BABEL_${l}"}"
-  if [ ! -e "${"BABEL_${l}"}" ]; then
-      log "Fill the value of 'BABEL_${l}' of db.sh"
+  echo "${BABEL}_${l}""
+  if [ ! -e ${BABEL}_${l} ]; then
+      log "Fill the value of ${BABEL}_${l} of db.sh"
       exit 1
   fi
 done
