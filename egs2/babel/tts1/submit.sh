3e4284df64c668c10b549da65159a1e6bae6c028
Zhaomou Song <zs323@cam.ac.uk>
2020-10-26 20:18:42 +0000

    change qsub script

--- egs2/babel/tts1/submit.sh
+++ egs2/babel/tts1/submit.sh
@@ -1,1 +1,1 @@
-qsub -M zs323@cam.ac.uk -m bea -S /bin/bash -l queue_priority=cuda-low,tests=0,mem_grab=0M,gpuclass=*,osrel=*,hostname=air208 ./run.sh -- stage 5 --stop-stage 5
+qsub -M zs323@cam.ac.uk -m bea -S /bin/bash -l queue_priority=cuda-low,tests=0,mem_grab=0M,gpuclass=*,osrel=*,hostname=air209 ./run.sh -- stage 6 --stop-stage 7
