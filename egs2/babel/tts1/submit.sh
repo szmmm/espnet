e37fd0ed9a869581d14bfa15255075e0097bc9bd
Zhaomou Song <zs323@cam.ac.uk>
2020-10-30 00:41:39 +0000

    changed FLP in prepare_data.sh

--- egs2/babel/tts1/submit.sh
+++ egs2/babel/tts1/submit.sh
@@ -1,1 +1,2 @@
-qsub -cwd -j y -o $LOG -l qp=cuda-low -l gpuclass='*' -l osrel='*' ./run.sh --stage 1 --stop-stage 2 --ngpu 4
+qsub -cwd -j y -o $LOG -l qp=low -l gpuclass='*' -l osrel='*' ./run.sh --stage 1 --stop-stage 2 --ngpu 4
+qsub -cwd -j yes -l qp=cuda-low -l gpuclass='*' -l osrel='*' ./run.sh --stage 1 --stop-stage 2
