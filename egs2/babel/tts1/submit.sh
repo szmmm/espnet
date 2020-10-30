510592a0b841ce56ed48c8d9d572fe727c2bdbab
Zhaomou Song <zs323@cam.ac.uk>
2020-10-30 00:21:46 +0000

    changed max-epoch and edited submit.sh

--- egs2/babel/tts1/submit.sh
+++ egs2/babel/tts1/submit.sh
@@ -1,1 +1,1 @@
-qsub -cwd -j y -o $LOG -l qp=cuda-low -l gpuclass='*' -l osrel='*' ./run.sh --stage 6 --stop-stage 7 --ngpu 4
+qsub -cwd -j y -o $LOG -l qp=cuda-low -l gpuclass='*' -l osrel='*' ./run.sh --stage 1 --stop-stage 2 --ngpu 4
