b32595d9858d5b6adfaef1eabbc0524c0e17453e
Zhaomou Song <zs323@cam.ac.uk>
2020-11-02 00:31:22 +0000

    changes sox dither rate to 8000

--- egs2/babel/tts1/local/data.sh
+++ egs2/babel/tts1/local/data.sh
@@ -41,7 +41,7 @@ test_set=${test_set%% }
 
 ./local/setup_languages.sh --langs "${langs}" --test "${test}"
 for x in ${train_set} ${train_dev} ${test_set}; do
-   sed -i.bak -e "s/$/ sox -R -t wav - -t wav - rate 16000 dither | /" data/${x}/wav.scp
+   sed -i.bak -e "s/$/ sox -R -t wav - -t wav - rate 8000 dither | /" data/${x}/wav.scp
 done
 
 cut -f 2- data/${train_set}/text | tr " " "\n" | sort | uniq | grep "<" > data/nlsym.txt
